5b5e5ceda03dee1db5d4c7c0407f58a1658072f3
codewolf86 <chris@codewolfconsulting.com>
2022-03-16 14:38:21 +0000

    Code Generators Workaround

---
+++ src/SFA.DAS.ApprenticeFeedback.Jobs/Functions/TemporaryGeneratedFunction.cs
@@ -0,0 +1,26 @@
+﻿// FunctionEndpointTrigger
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ApprenticeFeedback.Jobs.Functions
+{
+	internal class FunctionEndpointTrigger
+	{
+		private readonly IFunctionEndpoint endpoint;
+
+		public FunctionEndpointTrigger(IFunctionEndpoint endpoint)
+		{
+			this.endpoint = endpoint;
+		}
+
+		[FunctionName("ApprenticeFeedbackJobs")]
+		public Task Run([ServiceBusTrigger("SFA.DAS.ApprenticeFeedback", AutoCompleteMessages = false)] ServiceBusReceivedMessage message, ServiceBusClient client, ServiceBusMessageActions messageActions, ILogger logger, ExecutionContext executionContext)
+		{
+			return endpoint.ProcessAtomic(message, executionContext, client, messageActions, logger);
+		}
+	}
+}
