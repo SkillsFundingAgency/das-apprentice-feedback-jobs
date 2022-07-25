fa4eb686a1ca5d74055a6d6cd1c9d8dde3b1ca3a
Ian Rose <ian.rose@digital.education.gov.uk>
2022-07-25 15:02:17 +0100

    QF429 Feedback email transaction azure function

---
+++ src/SFA.DAS.ApprenticeFeedback.Jobs/Infrastructure/Api/Requests/PostGenerateEmailTransactionsRequest.cs
@@ -0,0 +1,9 @@
+﻿using SFA.DAS.ApprenticeFeedback.Jobs.Domain.Interfaces;
+
+namespace SFA.DAS.ApprenticeFeedback.Jobs.Infrastructure.Api.Requests
+{
+    public class PostGenerateEmailTransactionsRequest : IPostApiRequest
+    {
+        public string PostUrl => "feedbacktransaction";
+    }
+}
