055bfbfd0726ed34254df054ee4382a3578a3e21
Ian Rose <ian.rose@digital.education.gov.uk>
2022-07-25 15:02:17 +0100

    QF429 Feedback email transaction azure function

---
+++ src/SFA.DAS.ApprenticeFeedback.Jobs/Infrastructure/Api/Responses/GetEmailTransactionsResponse.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+
+namespace SFA.DAS.ApprenticeCommitments.Jobs.Api
+{
+    public class GetEmailTransactionsResponse
+    {
+        public int Count { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
