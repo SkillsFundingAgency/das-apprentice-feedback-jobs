50088d084e85252d9cf59a2e51b4f58f071f232f
codewolf86 <chris@codewolfconsulting.com>
2022-08-01 16:38:22 +0100

    Change Configuration

--- src/SFA.DAS.ApprenticeFeedback.Jobs/Domain/Configuration/NotificationsConfiguration.cs
+++ src/SFA.DAS.ApprenticeFeedback.Jobs/Domain/Configuration/NotificationsConfiguration.cs
@@ -1,13 +1,8 @@
 ﻿
-using System;
-
-
-#nullable disable
-
 namespace SFA.DAS.ApprenticeFeedback.Jobs.Domain.Configuration
 {
     public class NotificationsConfiguration
     {
-        public Guid FeedbackTransaction { get; set; }
+        public int FeedbackTransactionBatchSize { get; set; }
     }
 }
