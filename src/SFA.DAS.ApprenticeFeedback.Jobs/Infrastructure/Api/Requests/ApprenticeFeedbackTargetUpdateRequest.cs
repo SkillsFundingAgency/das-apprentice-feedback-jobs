19c03ec8c82f35c96a1d8906a4a02d635af70c8c
Matt <matt.daniels@digital.education.gov.uk>
2022-08-04 14:10:15 +0100

    QF-423 dev complete minus retry logic

---
+++ src/SFA.DAS.ApprenticeFeedback.Jobs/Infrastructure/Api/Requests/ApprenticeFeedbackTargetUpdateRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SFA.DAS.ApprenticeFeedback.Jobs.Infrastructure.Api.Requests
+{
+    public class ApprenticeFeedbackTargetUpdateRequest
+    {
+        public Guid ApprenticeFeedbackTargetId { get; set; }
+        public long ApprenticeshipId { get; set; }
+    }
+}
