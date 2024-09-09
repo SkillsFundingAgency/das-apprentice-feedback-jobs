9b14b8e0a4c2625f20110cdf36b239ddcbd27a8b
Chris Foster <chrisfoster186@googlemail.com>
2024-09-09 10:28:19 +0100

    Adds app insights logging

--- src/SFA.DAS.ApprenticeFeedback.Jobs/Program.cs
+++ src/SFA.DAS.ApprenticeFeedback.Jobs/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging.ApplicationInsights;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.ApprenticeFeedback.Jobs.Extensions;
 
 [assembly: NServiceBusTriggerFunction("SFA.DAS.ApprenticeFeedback.Jobs")]
@@ -14,6 +16,14 @@ var host = new HostBuilder()
             .AddApplicationInsightsTelemetryWorkerService()
             .ConfigureFunctionsApplicationInsights()
             .AddServiceRegistrations(context.Configuration);
+
+        services.AddLogging(options =>
+        {
+            options.AddApplicationInsights();
+            options.AddFilter<ApplicationInsightsLoggerProvider>("SFA.DAS", LogLevel.Information);
+            options.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Warning);
+
+        });
     })
     .Build();
 
